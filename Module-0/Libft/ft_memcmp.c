920ca58c9094b7934c1d374f90870672ab6437a4
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2024-10-29 10:46:10 +0000

    Add files via upload

---
+++ Module-0/Libft/ft_memcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpinho-c <cpinho-c@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-10-23 14:30:35 by cpinho-c          #+#    #+#             */
+/*   Updated: 2024-10-23 14:30:35 by cpinho-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	char	*str1;
+	char	*str2;
+
+	i = 0;
+	str1 = (char *) s1;
+	str2 = (char *) s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			break ;
+		else if (i == (n - 1))
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}
