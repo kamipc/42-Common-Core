5b6a31abc95f7578e1b570a0f5c93808a8c8f9d5
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2024-10-31 16:45:36 +0000

    created strdup

---
+++ Module-0/Libft/ft_strdup.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpinho-c <cpinho-c@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-10-31 15:50:45 by cpinho-c          #+#    #+#             */
+/*   Updated: 2024-10-31 15:50:45 by cpinho-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	size_t	i;
+
+	dest = (char *)malloc (ft_strlen(s) * sizeof(char) + 1);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
